ed00a86f88e587904f8faf6322d6d615c56a437c
Leo <nerd@whiteslash.eu>
2015-03-20 02:12:42 +0200

    Add basic folder navigation and history.

--- FileManager.js
+++ FileManager.js
@@ -1,7 +1,11 @@
-var fs = require('fs');
+var fs   = require('fs'),
+    mime = require('mime');
 
 var currentDirectory = '/home/leo/',
-    debug = true;
+    debug = true,
+    historyPosition = 0,
+    history = [];
+
 
 var UI = {
 	getLocation: function(){
@@ -25,6 +29,32 @@ var UI = {
 				callback(currentDirectory);
 			}
 		});
+	},
+	onFileClick: function( callback ) {
+		document.querySelector('#content')
+		.addEventListener('click', function( ev ) {
+			var target = ev.target,
+			    parent = target.parentNode;
+
+			console.log('click');
+
+			if (parent.classList.contains('item') )  {
+				console.log('click item');
+				callback( parent );
+			}
+		});
+	},
+	onPrevClick: function( callback ) {
+		document.querySelector('#prev-button')
+		.addEventListener('click', function( ev ) {
+			callback();
+		});
+	},
+	onNextClick: function( callback ) {
+		document.querySelector('#next-button')
+		.addEventListener('click', function( ev ) {
+			callback();
+		});
 	}
 };
 
@@ -33,28 +63,60 @@ currentDirectory = UI.getLocation();
 console.log("Test", currentDirectory);
 
 
+function getIconPath( iconName ) {
+	var iconTheme = 'Flattr';
+
+	var iconDirectory = '/usr/share/icons/';
+
+	return iconDirectory + iconTheme + '/' + iconName + '.svg';
+}
+
+function getMimeTypeIconName( fileName ) {
+	var mimeType = mime.lookup( fileName );
+
+	mimeType = mimeType.replace(/\//g, '-');
+
+	return 'mimetypes/48/' + mimeType;
+}
+
+function getFileTypeIconPath( file ) {
+	var iconName = '';
+
+	if ( file.stats.isFile() ) {
+		iconName = getMimeTypeIconName( file.fileName );
+	} else if ( file.stats.isDirectory() ) {
+		iconName = 'places/64/folder'
+	}
+
+	return getIconPath( iconName );
+}
+
 function File( options ) {
 	var that = this;
 
 	this.hidden = false;
+	this.stats  = null;
 
 	(function init( options ){
-		console.log("Options:", options);
+		// console.log("Options:", options);
 
 		if ( ! options || ! options.stats )
 			return null;
+
+		that.stats  = options.stats;
 
 		that.fileName = options.fileName;
 
 		if (that.fileName[0] === '.')
 			that.hidden = true;
 
-		console.log("Filename:", options.fileName);
+		// console.log("Filename:", options.fileName);
 	}( options ));
 
 	this.render = function() {
 		var fileElement = document.createElement('div');
 
+		var iconPath = getFileTypeIconPath(this);
 
 		var hiddenClass = '';
 
@@ -62,7 +124,8 @@ function File( options ) {
 			hiddenClass = ' hidden-item';
 
 		fileElement.className = 'item file' + hiddenClass;
-		fileElement.innerHTML = '<img src="/usr/share/icons/Flattr/places/64/folder.svg"><p>' + that.fileName + '</p>';
+		fileElement.innerHTML = '<img src="'+iconPath+'"><p>' + that.fileName + '</p>';
+		fileElement.obj = that;
 
 		return fileElement;
 	};
@@ -70,13 +133,13 @@ function File( options ) {
 
 
 function getFile(filePath, callback) {
-	console.log("filePath",filePath);
+	// console.log("filePath",filePath);
 
 	var absolutePath = currentDirectory + filePath;
-	console.log("absolutePath",absolutePath);
+	// console.log("absolutePath",absolutePath);
 
 	fs.stat(absolutePath, function(err,stats) {
-		console.log(err,stats);
+		// console.log(err,stats);
 
 		callback(
 			err,
@@ -89,17 +152,29 @@ function getFile(filePath, callback) {
 	});
 }
 
-function openDir( path ) {
+function openDir( path, resetHistory ) {
+	if ( resetHistory )
+		historyPosition = 0;
+
 	UI.clearFiles();
+	UI.setLocation( path );
+
+	if ( history.indexOf(path) === -1)
+		history.unshift( path );
+
+	console.log('History', history);
+
+	currentDirectory = path;
+
 
 	fs.readdir( path, function(err,files) {
 		"use strict";
 
 		var fileCount = files.length;
 
-		console.log("err",err);
-		console.log("files",files);
-		console.log("fileCount",fileCount);
+		// console.log("err",err);
+		// console.log("files",files);
+		// console.log("fileCount",fileCount);
 
 
 
@@ -116,12 +191,42 @@ function openDir( path ) {
 	});
 }
 
+function openPrevDir(){
+	historyPosition = Math.min(historyPosition+1, history.length -1);
+
+	openDir(history[historyPosition], false);
+};
+
+function openNextDir(){
+	historyPosition = Math.max(historyPosition-1, 0);
+
+	openDir(history[historyPosition], false);
+};
+
+
 function init() {
 	if ( debug ) {
 		require('nw.gui').Window.get().showDevTools();
 	}
 
 	UI.onLocationChange(function( path ){
+		openDir( path );
+	});
+
+	UI.onNextClick(function(){
+		openNextDir();
+	});
+
+	UI.onPrevClick(function(){
+		openPrevDir();
+	});
+
+	UI.onFileClick(function( element ) {
+		var path = UI.getLocation() + element.obj.fileName + '/';
+
+
+		console.log('Clicked', element);
+		console.log('Clicked', path);
 		openDir( path );
 	});
 
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-PACKAGE_NAME=nde-filemanger
+PACKAGE_NAME=nde-filemanager
 
 all: dev
 
@@ -6,7 +6,7 @@ all: dev
 kill:
 	-pkill nw
 
-dev: kill run
+dev: kill install run
 
 clean:
 	rm -rf dist/
@@ -15,6 +15,12 @@ build: clean
 	zip -r ../$(PACKAGE_NAME).nw *
 	mkdir -p dist
 	mv ../$(PACKAGE_NAME).nw dist/$(PACKAGE_NAME).nw
+
+install: build
+	install nde-filemanager.desktop ~/.local/share/applications
+	install nde-filemanager ~/bin
+	mkdir -p ~/.local/share/nde-filemanger
+	install dist/nde-filemanager.nw ~/.local/share/nde-filemanager
 
 run: build
 	nw dist/$(PACKAGE_NAME).nw
